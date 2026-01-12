36b3dbde51bdb72e8f8b468ac7b93e356de50894
thecozies <79979276+thecozies@users.noreply.github.com>
2026-01-12 15:58:59 -0600

    adjust launcher anim and launcher

--- include/banjo_launcher.h
+++ include/banjo_launcher.h
@@ -4,8 +4,13 @@
 #include "recompui/recompui.h"
 
 namespace banjo {
-    void launcher_animation_setup(recompui::LauncherMenu *menu);
+    void launcher_animation_setup(recompui::LauncherMenu *menu, recompui::Element *title);
     void launcher_animation_update(recompui::LauncherMenu *menu);
+
+    constexpr float launcher_options_right_position_start = 96.0f;
+    constexpr float launcher_options_right_position_end = 96.0f + 24.0f;
+    constexpr float launcher_options_top_offset = 96.0f;
+    constexpr float launcher_options_title_offset = 120.0f;
 }
 
 #endif
